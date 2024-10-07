9542b7b78e80c2669461cc79c080c7e604272f15
Emma <emmaolssondev@outlook.com>
2024-10-07 10:42:05 +0200

    add createmetod

--- Gruppövning_InternationalFoodAB/Classes/Recept_Handler.cs
+++ Gruppövning_InternationalFoodAB/Classes/Recept_Handler.cs
@@ -8,11 +8,39 @@ namespace Gruppövning_InternationalFoodAB.Classes
 {
     public class Recept_Handler
     {
+        public Guid Id { get; set; }
         public List<Recept> AllRecepts { get; set; }
+        private string path;
 
-        public Recept_Handler(List<Recept> allRecepts)
+        public Recept_Handler(List<Recept> allRecepts, string filePath)
         {
             AllRecepts = allRecepts;
+            path = filePath;
+        }
+
+        public void Create(Recept recept)
+        {
+            if (recept.Id == Guid.Empty)
+            {
+            }
+
+            string jsonRecepts = JsonSerializer.Serialize(recept);
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(path, true))
+                {
+                    sw.WriteLine(jsonRecepts);
+                }
+                //Visa medelande nånstans
+                //Console.WriteLine("Receptet har sparats.");
+            }
+            catch (Exception ex)
+            {
+                //Visa medelande nånstans
+                //Console.WriteLine($"Ett fel inträffade vid sparning av recept: {ex.Message}");
+            }
         }
     }
+
 }
