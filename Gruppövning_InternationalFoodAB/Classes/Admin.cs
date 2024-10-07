46ee8f69063e073d79b6a03ce7ec3b23b3eb20f7
Viv Madigan <vivyanmadigan@gmail.com>
2024-10-07 10:27:27 +0200

    Jag har skapat Admin och Guest klasser, bara properties, inga metoder

---
+++ Gruppövning_InternationalFoodAB/Classes/Admin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gruppövning_InternationalFoodAB.Classes
+{
+    public class Admin
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public Admin(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+    }
+}
