29a43f99d92fbec4293d369e4b0401d983063ee4
Hanna M <hanna-m95@hotmail.com>
2024-10-07 11:44:05 +0200

    Lagt till property Id

--- Gruppövning_InternationalFoodAB/Classes/Recept.cs
+++ Gruppövning_InternationalFoodAB/Classes/Recept.cs
@@ -5,12 +5,14 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public string TypeOfRecept {  get; set; }
+        public Guid Id { get; set; }
 
-        public Recept(string name, string description, string typeOfRecept)
+        public Recept(string name, string description, string typeOfRecept, Guid id)
         {
             Name = name;
             Description = description;
             TypeOfRecept = typeOfRecept;
+            Id = id;
         }
     }
 }
