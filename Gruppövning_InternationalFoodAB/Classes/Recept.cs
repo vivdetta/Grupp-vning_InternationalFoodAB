afb0080e1df00d8f9cbd71e2aab3723d30a5d2cc
Hanna M <hanna-m95@hotmail.com>
2024-10-07 10:28:30 +0200

    Receptklass med properties och konstruktor

--- Gruppövning_InternationalFoodAB/Classes/Recept.cs
+++ Gruppövning_InternationalFoodAB/Classes/Recept.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Gruppövning_InternationalFoodAB.Classes
+﻿namespace Gruppövning_InternationalFoodAB.Classes
 {
     public class Recept
     {
@@ -19,5 +13,4 @@ namespace Gruppövning_InternationalFoodAB.Classes
             TypeOfRecept = typeOfRecept;
         }
     }
-
 }
